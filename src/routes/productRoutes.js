d829b5a12a0bc03c542ee5c121721b008c9655e4
breninx1 <id.57497094858@gmail.com>
2024-09-26 13:33:18 -0300

    atualização2

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,14 @@
-import { Router } from 'express';
-import { getProducts } from '../controllers/productController.js';
+import {Router} from 'express';
+import {getProducts, createProductController, getProduct, updateProductController, deleteProductController} from '../controllers/productController.js';
+import { deleteProduct } from '../services/productService.js';
+
 
 const router = Router();
 
 router.get("/products", getProducts);
+router.get("/products/:id", getProduct);
+router.post("/products", createProductController);
+router.patch("/products/:id", updateProductController);
+router.delete("/products/:id", deleteProductController);
 
 export default router;
